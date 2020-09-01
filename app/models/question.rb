cc729ab6fe3996c4f8b0cd60578f10517221eefd
40grivenprog <grivenmus40@yandex.ru>
2020-09-01 11:49:21 +0300

    Fix notes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,6 @@
 class Question < ApplicationRecord
-
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
-
 end
