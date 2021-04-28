c58f45efd02153590a6983e51912004f81e855e3
40grivenprog <grivenmus40@yandex.ru>
2021-04-28 19:21:41 +0300

    Fixed ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
+set :rvm_ruby, "2.7.3"
 
 set :application, "qna"
 set :repo_url, "git@github.com:40grivenprog/qna.git"
@@ -13,5 +14,3 @@ append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
-after 'deploy:publishing', 'unicorn:restart'
-
